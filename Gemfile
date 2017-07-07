1a85522a041c1ae635908eaf55cf4e6942c71557
Milan Barande <milan.barande@hotmail.fr>
2017-07-07 11:50:59 -0400

    photo uploading works

--- Gemfile
+++ Gemfile
@@ -62,5 +62,7 @@ gem 'simple_form'
 gem 'autoprefixer-rails'
 
 gem 'figaro'
+gem 'cloudinary'
+gem 'carrierwave', '~> 0.11.2'
 
 ruby '2.3.3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:new, :index, :show, :create ] do
+  resources :cocktails, only: [:new, :index, :show, :create, :delete ] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
